3480261c41c70cce519f44400bbd065ecffb390e
Andrew Lu <omegaw@outlook.com>
2023-07-09 14:47:14 -0700

    updated knexfile with new settings

--- src/utility/EnvironmentValidatior.ts
+++ src/utility/EnvironmentValidatior.ts
@@ -6,10 +6,7 @@ const envSchema = zod.object({
     NODE_ENV: zod.string().nonempty(),
     EMAIL_SECRET_KEY: zod.string().nonempty(),
     BASE_URL: zod.string().nonempty(),
-    DATABASE_PORT: zod.string().nonempty(),
-    DATABASE_HOST: zod.string().nonempty(),
-    DATABASE_NAME: zod.string().nonempty(),
-    DATABASE_USER: zod.string().nonempty(),
-    DATABASE_ACCESS_KEY: zod.string().nonempty(),
+    CONNECTION_URL: zod.string().nonempty(),
+
 });
 export const env = envSchema.parse(process.env);
