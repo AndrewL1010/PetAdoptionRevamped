cd5bc5e1deeb31d7cde7d8bd2ed151bcbd9322b7
Andrew Lu <omegaw@outlook.com>
2023-07-09 15:09:04 -0700

    updated knexfile with new settings

--- src/app/database/knexfile.js
+++ src/app/database/knexfile.js
@@ -6,7 +6,7 @@ import { env } from '../../utility/EnvironmentValidatior';
  */
 module.exports = {
   client: 'pg',
-  connection: "postgres://andrew_lu:qL7k8oUBBYYjqZQhVTRCy30ezUaLSvdm@dpg-cili7nlgkuvinfl3edc0-a.oregon-postgres.render.com/pet_sanctuary",
+  connection: "postgres://andrew_lu:qL7k8oUBBYYjqZQhVTRCy30ezUaLSvdm@dpg-cili7nlgkuvinfl3edc0-a.oregon-postgres.render.com/pet_sanctuary?ssl=true",
   debug: true,
   migrations: {
     directory: "./migrations"
