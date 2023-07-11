6c33a5d48ddb3e21a90739347a376b8eed024b70
Andrew Lu <omegaw@outlook.com>
2023-07-10 23:06:21 -0700

    added authentication when user tried to adopt an animal without logging in first

--- src/app/pets/page.tsx
+++ src/app/pets/page.tsx
@@ -4,28 +4,13 @@ import { cookies } from 'next/headers';
 import { Animal } from '@/types/TableModels';
 async function Page() {
     const database = getConnection();
-    function getFilter(): string {
-
-        const filter = cookies().get("filter");
-        if (!filter) {
-            return "all";
-        }
-        else {
-            try {
-                return filter.value;
-            }
-            catch (error) {
-                return "all";
-            }
-        }
-    }
+    
     if (database) {
         const animals = await database<Animal>('animals');
         await database.destroy();
-        const filter = getFilter();
         if (animals) {
             return (
-                <FilterComponent animals={animals} filter={filter ? filter : "all"}></FilterComponent>
+                <FilterComponent animals={animals}></FilterComponent>
             )
         }
 
